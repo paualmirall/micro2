2ea5bda761458b736eff860d76b195eacada415b
Pau Almirall <pau.almirall@lidl.es>
2019-06-19 15:33:48 +0200

    add externals

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? 'https://paualmirallmicro2.netlify.com/'
-      : '/'
+      : '/',
+    chainWebpack: config => {
+      config.externals({
+        lodash: 'lodash',
+        vue: 'vue',
+        'core-js': 'core-js'
+      })
+    }
   }
