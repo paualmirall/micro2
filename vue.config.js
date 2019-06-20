9b6c16006772e37a2bc05d430884acf23e8890ca
Pau Almirall <pau.almirall@lidl.es>
2019-06-20 10:42:24 +0200

    use webpack plugin

--- vue.config.js
+++ vue.config.js
@@ -7,14 +7,10 @@ module.exports = {
     configureWebpack: {
       plugins: [
         new WebpackCdnPlugin({
-          modules: {
-            'vue': [
-              { name: 'vue', var: 'Vue', path: 'dist/vue.runtime.min.js' },
-            ],
-            'lodash': [
-              { name: 'lodash', var: '_', path: 'lodash.min.js' },
-            ]
-          },
+          modules: [
+            { name: 'vue', var: 'Vue', path: 'dist/vue.runtime.min.js' },
+            { name: 'lodash', var: '_', path: 'lodash.min.js' },
+          ],
         }),
       ]
     }
